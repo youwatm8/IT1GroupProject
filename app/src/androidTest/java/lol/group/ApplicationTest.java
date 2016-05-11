d9f0280561eaf84f91ecb938a038ecb803b0c545
aaron.carmen <le.twitch153@gmail.com>
2016-05-11 14:14:55 -0500

    even more shit

--- app/src/androidTest/java/lol/group/ApplicationTest.java
+++ app/src/androidTest/java/lol/group/ApplicationTest.java
@@ -2,12 +2,31 @@ package lol.group;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
-
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+
+
+
+
     public ApplicationTest() {
         super(Application.class);
+
     }
 }
